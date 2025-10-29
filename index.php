89b6770366f4bb8c1dfe68229e56c3c176a53201
M. Raihan Ramadhani <raihanrasengan123@gmail.com>
2025-10-29 07:15:36 +0700

    Add files project DevOps

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Hello from PHP App!</h1>";
+echo "<p>Nama: M. Raihan Ramadhani</p>";
+echo "<p>NIM: 2210512067</p>";
+echo "<p>Kelas: Cloud Computing - A</p>";
+echo "<p>Deployed via DevOps CI/CD pipeline</p>";
+?>
